5f15a7f9c6951542e88de272a4913287737353d1
unknown <matea@goranb2.edusoft2.com>
2015-06-24 15:01:01 +0200

    Edited

--- skripta.sh
+++ skripta.sh
@@ -7,10 +7,10 @@ fi
 
 file=$1
 user=$2
-time=`cat $file | sed -n '/[0-9][0-9]:[0-9][0-9]:[0-9][0-9] */p' | grep '/' | awk '{ printf "%-15s %-200s %-7s\n", $2, $5, $14 }' | awk '{ o=$1 
+time=`grep '^[^#]' $file | awk '{ printf "%-15s %-15s %-200s %-7s\n", $1, $2, $5, $14 }' | awk '{ o=$2 
 	gsub(/[^0-9]:/,"",o)
 	tmp=sprintf("%02s", o+2)
-	$1=tmp substr($1,3, length($1))  
+	$2=tmp substr($2,3, length($2))  
 	}1'`
 
 echo $time
