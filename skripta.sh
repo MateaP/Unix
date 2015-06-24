a3c2c7e913d1cc21cd535dfe554d4d31acdf32ec
unknown <matea@goranb2.edusoft2.com>
2015-06-24 16:16:04 +0200

    nova verzija

--- skripta.sh
+++ skripta.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-if [ "$#" -lt 2 ]
+if [ "$#" -lt 1 ]
 then
-	echo "USAGE: $0 logFile user"
+	echo "USAGE: $0 logFile"
 	exit 1
 fi
 
 file=$1
-user=$2
-time=`awk '!/^ *#/ { print $1, $2, $5, $14; }' $file | awk '{ o=$2 
+time=`awk '!/^ *#/ { o=$2 
 	gsub(/[^0-9]:/,"",o)
 	tmp=sprintf("%02s", o+2)
-	$2=tmp substr($2,3, length($2))  
-	}1'`
+	$2=$1 " " tmp substr($2,3, length($2)) " " $5 " " $14; }1 { print $2; }' $file`
 
 echo $time
 
